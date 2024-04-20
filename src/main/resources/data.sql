be9f56acd038d04da4ea4797455a4322067463b2
101 <tmddnjs001133@dsm.hs.kr>
2024-04-20 23:34:24 +0900

    add :: data.sql
    insert native query

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into park_minju(id, name) values (1, 'Minju Park 1');
+insert into park_minju(id, name) values (2, 'Minju Park 2');
+insert into park_minju(id, name) values (3, 'Minju Park 3');
+insert into park_minju(id, name) values (4, 'Minju Park 4');
+insert into park_minju(id, name) values (5, 'Minju Park 5');
